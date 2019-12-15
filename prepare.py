dd35b5f6de500809ea5d1ce9bff2ca54febc73c6
Camila <deycy-camila.arias-villamil@imt-atlantique.net>
2019-12-15 19:00:00 +0100

    add dataset not pathological, validate_model

--- prepare.py
+++ prepare.py
@@ -114,7 +114,7 @@ def generate_cpatches(nsamples, sample_rates=[0.5, 0.5], add_fake=False, limits=
         yield X_all[shuffle_idx], y_all[shuffle_idx]
 """
 def generate_cpatches(nsamples, patches_path ='./patches.npz' ,ctype=None):
-    
+    print("g",nsamples)
     combined_dims = 4
 
     while True:
@@ -154,5 +154,52 @@ def generate_cpatches(nsamples, patches_path ='./patches.npz' ,ctype=None):
             X_.append(X_combined)
         X_ = np.stack(X_)
         print("Patches size {},{}".format(X_.shape,y_group.shape))
+        shuffle_idx = np.random.choice(len(X_), len(X_), replace=False)
+        yield X_[shuffle_idx], y_group[shuffle_idx]
+
+def generate_nc_patches(nsamples, patches_path ='./patches.npz',
+                        cancer_path = './non_cancer_patches.npz',
+                        ctype=None):
+    print(nsamples)
+    combined_dims = 4
+
+    while True:
+        counts = nsamples
+        #n_samples -> n_experiments, probabity sample_rates
+        X_all = None
+        #load all images from each class (i)
+        X_masks = np.load(patches_path)['x_mask']
+        X_reals = np.load(cancer_path)['x_real']
+        try:
+            # get (c) images
+            shuffle_idx_mask = np.random.choice(len(X_masks), counts, replace=False)
+            shuffle_idx_cancer  = np.random.choice(len(X_reals), counts, replace=False)
+        except:
+            pdb.set_trace()
+        
+        #get mask and reals
+        X_masks = X_masks[shuffle_idx_mask]
+        X_reals = X_reals[shuffle_idx_cancer]
+        label = np.array([0,1]) #always pathological
+        y_group = np.repeat(label.reshape((1, 1, 1, -1)), counts, axis=0)
+
+        X_ = []
+        for i,_ in enumerate(X_masks):
+            X_mask = X_masks[i].reshape((patch_size, patch_size))
+            X_real = normalize(X_reals[i].reshape((patch_size, patch_size)))
+
+            X_rand = np.random.uniform(0, 1, X_mask.shape)*X_mask
+            X_corrupt = (np.multiply(X_real, np.logical_not(X_mask).astype(int))+X_rand)
+            boundary = np.multiply(np.invert(morph.binary_erosion(X_mask)), X_mask)
+            X_boundary = normalize(filters.gaussian_filter(255.0*boundary,10)).reshape((patch_size, patch_size, 1))
+
+            X_combined = np.concatenate((X_corrupt.reshape(patch_size, patch_size, 1), 
+                                        X_mask.reshape(patch_size, patch_size, 1),
+                                        X_real.reshape(patch_size, patch_size, 1),
+                                        X_boundary), 
+                                        axis=-1)
+            X_.append(X_combined)
+        X_ = np.stack(X_)
+        print("Patches_ncexirt size {},{}".format(X_.shape,y_group.shape))
         shuffle_idx = np.random.choice(len(X_), len(X_), replace=False)
         yield X_[shuffle_idx], y_group[shuffle_idx]
--- train.py
+++ train.py
@@ -232,11 +232,38 @@ class GENGAN:
     def validate_model(self):
         with tf.compat.v1.Session() as self.sess:
             self.sess.run(tf.compat.v1.global_variables_initializer())
-            self.saver.restore(self.sess, models_dir + self.save_name)
-            data_generator_val = generate_cpatches(self.batch_size, ctype=data_type)
+            if self.ckpt_num is not None:
+                self.g_saver.restore(self.sess, checkpoints_dir + self.load_name+'_'+str(self.ckpt_num))
+                self.d_saver.restore(self.sess, checkpoints_dir + self.load_name+'_'+str(self.ckpt_num))
+            else:
+                self.g_saver.restore(self.sess, models_dir + self.load_name)
+                self.d_saver.restore(self.sess, models_dir + self.load_name)
+            generator_syn =  generate_cpatches(batch_size, ctype=data_type)
 
-            # Save some random validation samples
-            self.validate(1,data_generator_val, self.sess)
+            self.validate(1, generator_syn, self.sess)
+        #     generator_syn = generate_nc_patches(self.batch_size, ctype=data_type)
+            
+        #     data_X, data_c = next(generator_syn)
+        #     data_x = data_X[0:1, :, :, 0:1]
+        #     data_mask = data_X[0:1, :, :, 1:2]
+        #     data_real = data_X[0:1, :, :, 2:3]
+        #     input_image = data_x[0, :, :, 0]
+        #     real_image = data_real[0, :, :, 0]
+
+        #     pred_img = self.sess.run(self.fake_image, feed_dict={
+        #             self.input_x: data_x,
+        #             self.input_mask: data_mask,
+        #             self.input_c: data_c
+        #            })
+        #     pred_img = pred_img[0, :, :, 0]
+        #     img = np.concatenate((real_image,input_image, pred_img), axis=1)
+        #     img = scipy.ndimage.zoom(img, zoom=[0.75, 0.75])
+        #     directory = './synthesis/' + self.save_name + '/'
+        #     if not os.path.exists(directory):
+        #         os.makedirs(directory)
+        #     print(np.unique(pred_img))
+        #     scipy.misc.toimage(img, cmin=0.0, cmax=1.0).save(directory + self.save_name + '_' + str(self.patch_size) + '_' + str(i) + '.png')
+        # tf.compat.v1.reset_default_graph()
 
     def synthesize_dataset(self, num, batch_size=batch_size, limits=[None]*c_dims, sample_rates = [0.5, 0.5]):
         with tf.compat.v1.Session() as self.sess:
@@ -250,7 +277,7 @@ class GENGAN:
                 self.d_saver.restore(self.sess, models_dir + self.load_name)
 
             # Create patch generator
-            generator_syn = generate_cpatches(batch_size, ctype=data_type)
+            generator_syn = generate_nc_patches(batch_size, ctype=data_type)
 
             X_train = np.zeros((num, batch_size, self.patch_size, self.patch_size, 1))
             y_train = np.zeros((num, batch_size, c_dims))
@@ -259,8 +286,6 @@ class GENGAN:
                 data_X, data_c = next(generator_syn)
 
                 # Use normals to generate malignant, and vice versa
-                data_c[:, :, :, 0] = 1-data_c[:, :, :, 0]
-                data_c[:, :, :, 1] = 1-data_c[:, :, :, 1]
                 data_x = data_X[:, :, :, 0:1]
                 data_mask = data_X[:, :, :, 1:2]
                 pred_img = self.sess.run(self.fake_image, feed_dict={
@@ -275,7 +300,8 @@ class GENGAN:
             y_train = y_train.reshape((-1, c_dims))
             return X_train, y_train
     
-    def synthesize_image(self, num, batch_size=batch_size, limits=[None]*c_dims, sample_rates = [0.5, 0.5]):
+    def synthesize_image(self, num):
+        tf.compat.v1.reset_default_graph()
         with tf.compat.v1.Session() as self.sess:
             # Load model
             self.sess.run(tf.compat.v1.global_variables_initializer())
