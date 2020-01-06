2aadbf539ad61f53803fb8621fbb0ae05d16f005
Camila <deycy-camila.arias-villamil@imt-atlantique.net>
2020-01-06 17:34:24 +0100

    change t1

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+#version T1 to 
 from prepare import *
 from model_utils import *
 from config import *
@@ -16,13 +17,13 @@ from time import gmtime, strftime
 from sys import argv
 import hickle as hkl
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-tf.compat.v1.disable_eager_execution()
+
+
 class GENGAN:
 
     def __init__(self, save_name, load_name, patch_size, num_iterations,
                  batch_size, new_model, sample_rates, ckpt_num, train_vgg, load_vgg, 
                  load_weights, limits,use_c):
-        self.sess = None
         self.save_name = save_name
         self.patch_size = patch_size
         self.load_name = load_name
@@ -55,89 +56,88 @@ class GENGAN:
 
     def build_model(self):
         print('Building model')
-        
+
         # Learning rate params
         self.global_step = tf.Variable(0, trainable=False)
-        self.learn_rate = tf.compat.v1.train.exponential_decay(learn_rate, self.global_step,
+        self.learn_rate = tf.train.exponential_decay(learn_rate, self.global_step,
                                                      500, 0.99, staircase=False)
 
         # Input variables
-        self.input_x = tf.compat.v1.placeholder(tf.float32, [None, self.patch_size, self.patch_size, 1])
-        self.input_mask = tf.compat.v1.placeholder(tf.float32, [None, self.patch_size, self.patch_size, 1])
-        self.input_real = tf.compat.v1.placeholder(tf.float32, [None, self.patch_size, self.patch_size, 1])
-        self.input_boundary = tf.compat.v1.placeholder(tf.float32, [None, self.patch_size, self.patch_size, 1])
-        self.input_c = tf.compat.v1.placeholder(tf.float32, [None, 1, 1, c_dims])
-        self.input_z = tf.compat.v1.placeholder(tf.float32, [None, 1, 1, z_dims])
+        self.input_x = tf.placeholder(tf.float32, [None, self.patch_size, self.patch_size, 1])
+        self.input_mask = tf.placeholder(tf.float32, [None, self.patch_size, self.patch_size, 1])
+        self.input_real = tf.placeholder(tf.float32, [None, self.patch_size, self.patch_size, 1])
+        self.input_boundary = tf.placeholder(tf.float32, [None, self.patch_size, self.patch_size, 1])
+        self.input_c = tf.placeholder(tf.float32, [None, 1, 1, c_dims])
+        self.input_z = tf.placeholder(tf.float32, [None, 1, 1, z_dims])
         self.fake_image = build_generator(self.input_x, self.input_mask, self.input_c, use_c=self.use_c)
 
-        #
         # Build discriminator
         self.discriminator = build_discriminator(input_x=self.input_x, input_c=self.input_c, use_c=self.use_c)
         D_real, self.D_real_attr = self.discriminator([self.input_real, self.input_c])
         D_fake, _ = self.discriminator([self.fake_image, self.input_c])
 
         # Set training variables
-        self.t_vars = tf.compat.v1.trainable_variables()
+        self.t_vars = tf.trainable_variables()
         self.d_vars = [ var for var in self.t_vars if var.name.startswith('d_')]
         self.g_vars = [ var for var in self.t_vars if var.name.startswith('g_')]
         self.c_vars = [ var for var in self.t_vars if var not in self.d_vars and var not in self.g_vars]
 
         # Set savers
-        self.saver = tf.compat.v1.train.Saver(self.g_vars + self.d_vars, max_to_keep=10000)
-        self.g_saver = tf.compat.v1.train.Saver(self.g_vars)
-        self.d_saver = tf.compat.v1.train.Saver(self.d_vars)
+        self.saver = tf.train.Saver(self.g_vars + self.d_vars, max_to_keep=10000)
+        self.g_saver = tf.train.Saver(self.g_vars)
+        self.d_saver = tf.train.Saver(self.d_vars)
 
         # Set loss functions
-        self.G_loss = tf.reduce_mean(input_tensor=tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake, labels=tf.ones_like(D_real)))
-        self.D_loss = (tf.reduce_mean(input_tensor=tf.nn.sigmoid_cross_entropy_with_logits(logits=D_real, labels=tf.ones_like(D_real))) + tf.reduce_mean(input_tensor=tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake, labels=tf.zeros_like(D_fake)))) / 2.0
+        self.G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake, labels=tf.ones_like(D_real)))
+        self.D_loss = (tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_real, labels=tf.ones_like(D_real))) + tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake, labels=tf.zeros_like(D_fake)))) / 2.0
 
         # Set solvers
-        self.G_solver = tf.compat.v1.train.AdamOptimizer(learning_rate=self.learn_rate, beta1=0.9, beta2=0.999, epsilon=1e-08).minimize(self.G_loss, var_list=self.g_vars, global_step=self.global_step)
-        self.D_solver = tf.compat.v1.train.AdamOptimizer(learning_rate=self.learn_rate, beta1=0.9, beta2=0.999, epsilon=1e-08).minimize(self.D_loss, var_list=self.d_vars, global_step=self.global_step)
+        self.G_solver = tf.train.AdamOptimizer(learning_rate=self.learn_rate, beta1=0.9, beta2=0.999, epsilon=1e-08).minimize(self.G_loss, var_list=self.g_vars, global_step=self.global_step)
+        self.D_solver = tf.train.AdamOptimizer(learning_rate=self.learn_rate, beta1=0.9, beta2=0.999, epsilon=1e-08).minimize(self.D_loss, var_list=self.d_vars, global_step=self.global_step)
 
         # Build VGG networks
         vgg_real_c = build_vgg19(tf.multiply(self.input_real, 1 - self.input_mask))
         vgg_fake_c = build_vgg19(tf.multiply(self.fake_image, 1 - self.input_mask), reuse=True)
 
         # Extract VGG weights
-        self.G_loss_vgg = tf.reduce_mean(input_tensor=tf.abs(vgg_real_c['input'] - vgg_fake_c['input']))
+        self.G_loss_vgg = tf.reduce_mean(tf.abs(vgg_real_c['input'] - vgg_fake_c['input']))
         vgg_real = build_vgg19(tf.multiply(self.input_real, self.input_mask))
         vgg_fake = build_vgg19(tf.multiply(self.fake_image, self.input_mask), reuse=True)
-        self.G_loss_vgg += tf.reduce_mean(input_tensor=tf.abs(vgg_real['input'] - vgg_fake['input']))
+        self.G_loss_vgg += tf.reduce_mean(tf.abs(vgg_real['input'] - vgg_fake['input']))
         for i in range(1, 4):
             conv_str = 'pool' + str(i)
-            self.G_loss_vgg += tf.reduce_mean(input_tensor=tf.abs(vgg_real[conv_str] - vgg_fake[conv_str]))
+            self.G_loss_vgg += tf.reduce_mean(tf.abs(vgg_real[conv_str] - vgg_fake[conv_str]))
 
         vgg_real = build_vgg19(tf.multiply(self.input_real, self.input_boundary))
         vgg_fake = build_vgg19(tf.multiply(self.fake_image, self.input_boundary), reuse=True)
-        self.G_loss_vgg += tf.reduce_mean(input_tensor=tf.abs(vgg_real['input'] - vgg_fake['input']))
+        self.G_loss_vgg += tf.reduce_mean(tf.abs(vgg_real['input'] - vgg_fake['input']))
         for i in range(1, 4):
             conv_str = 'pool' + str(i)
-            self.G_loss_vgg += tf.reduce_mean(input_tensor=tf.abs(vgg_real[conv_str] - vgg_fake[conv_str]))
+            self.G_loss_vgg += tf.reduce_mean(tf.abs(vgg_real[conv_str] - vgg_fake[conv_str]))
 
         # Set VGG solver
-        self.VGG_solver = tf.compat.v1.train.AdamOptimizer(learning_rate=self.learn_rate).minimize(self.G_loss_vgg, global_step=self.global_step, var_list=self.g_vars)
+        self.VGG_solver = tf.train.AdamOptimizer(learning_rate=self.learn_rate).minimize(self.G_loss_vgg, global_step=self.global_step, var_list=self.g_vars)
 
         # L1 and boundary loss
         self.L1_loss = self.l1_factor * \
-                       tf.reduce_mean(input_tensor=tf.abs(self.alpha *
+                       tf.reduce_mean(tf.abs(self.alpha *
                        tf.multiply(self.input_mask, self.fake_image - self.input_real)) +
                        tf.abs((1 - self.alpha) *
                        tf.multiply(1 - self.input_mask, self.fake_image - self.input_real)))
 
-        self.L1_solver = tf.compat.v1.train.AdamOptimizer(learning_rate=self.learn_rate).minimize(self.L1_loss, global_step=self.global_step, var_list=self.g_vars)
-        self.boundary_loss = self.boundary_factor * tf.reduce_mean(input_tensor=tf.multiply(self.input_boundary, tf.abs(self.fake_image - self.input_real)))
-        self.boundary_solver = tf.compat.v1.train.AdamOptimizer(learning_rate=self.learn_rate).minimize(self.boundary_loss,
+        self.L1_solver = tf.train.AdamOptimizer(learning_rate=self.learn_rate).minimize(self.L1_loss, global_step=self.global_step, var_list=self.g_vars)
+        self.boundary_loss = self.boundary_factor * tf.reduce_mean(tf.multiply(self.input_boundary, tf.abs(self.fake_image - self.input_real)))
+        self.boundary_solver = tf.train.AdamOptimizer(learning_rate=self.learn_rate).minimize(self.boundary_loss,
                                                                                               global_step=self.global_step,
                                                                                               var_list=self.g_vars)
 
     def train_model(self):
-        with tf.compat.v1.Session() as self.sess:
-            self.sess.run(tf.compat.v1.global_variables_initializer())
+        with tf.Session() as self.sess:
+            self.sess.run(tf.global_variables_initializer())
             # If using existing model
             if not self.new_model:
                 # Load the VGG loss trained model
-                if self.load_vgg and tf.compat.v1.train.checkpoint_exists(models_dir + self.save_name + '_vgg'):
+                if self.load_vgg and tf.train.checkpoint_exists(models_dir + self.save_name + '_vgg'):
                     print('Loading vgg')
                     self.g_saver.restore(self.sess, models_dir + self.save_name + '_vgg')
                 # Load the GAN loss trained model
@@ -155,15 +155,17 @@ class GENGAN:
             print('Getting dataset')
             data_generator = generate_cpatches(self.batch_size, ctype=data_type)
             val_data_generator = generate_cpatches(1, ctype=data_type)
+
             # iteration number
             it = 0
-            # number of VGG loss pre-training iterations (orig 100)
-            vgg_iters = 10
+            # number of VGG loss pre-training iterations
+            vgg_iters = 100
             # Number of iterations per epoch for each type of loss
             d_iters = 5
             g_iters = 1
             boundary_iters = 10
-            max_iters = 200 #orig5000
+            vgg_iters = 10
+            max_iters = 5000
             # Alternatively, train the D or G until loss drops below threshold
             D_loss_threshold = 0.3
             G_loss_threshold = 0.3
@@ -173,13 +175,14 @@ class GENGAN:
             if self.train_vgg:
                 print('Pre-training on VGG')
                 for i in range(vgg_iters):
-                    print('Iteration', i,vgg_iters) #org(10)
-                    VGG_loss_cur = self.train(self.sess, self.VGG_solver, self.G_loss_vgg, generator=data_generator, iters=5)
+                    print('Iteration', i,vgg_iters)
+                    VGG_loss_cur = self.train(self.sess, self.VGG_solver, self.G_loss_vgg, generator=data_generator, iters=10)
                     print('VGG loss', VGG_loss_cur)
                     self.validate(i, val_data_generator, self.sess)
                     save(self.save_name + '_vgg', it, self.g_saver, self.sess)
 
             while it < int(self.num_iterations):
+
                 it += 1
                 for j in range(1):
                     D_timer = 0
@@ -198,6 +201,7 @@ class GENGAN:
                         print('G loss', G_loss_cur)
                         it += g_iters
                         G_timer += 1
+
                     print('========')
 
                 # VGG LOSS
@@ -213,13 +217,11 @@ class GENGAN:
                     boundary_loss_cur = self.train(self.sess, self.boundary_solver, self.boundary_loss, generator=data_generator, iters=boundary_iters)
                     it += boundary_iters
                     print('Boundary loss', boundary_loss_cur)
-		#L1 LOSS
-
+                #L1 LOSS
                 for i in range(1):
-                    L1_loss_cur = self.train(self.sess, self.L1_solver, self.L1_loss, generator=data_generator, iters=5)
+                    L1_loss_cur = self.train(self.sess, self.L1_solver, self.L1_loss, generator=data_generator, iters=boundary_iters)
                     it += boundary_iters
                     print('L1 loss', L1_loss_cur)
-
                 # Save model some of the time
                 if np.random.random() > 0.25:
                     print('Saving model')
@@ -231,12 +233,11 @@ class GENGAN:
 
             print('Saving model')
             save(self.save_name, it, self.saver, self.sess)
-            #np.savez_compressed('data_loss',d=d_,v=v_,g=g_)
-        tf.compat.v1.reset_default_graph()
+        tf.reset_default_graph()
 
     def validate_model(self):
-        with tf.compat.v1.Session() as self.sess:
-            self.sess.run(tf.compat.v1.global_variables_initializer())
+        with tf.Session() as self.sess:
+            self.sess.run(tf.global_variables_initializer())
             if self.ckpt_num is not None:
                 self.g_saver.restore(self.sess, checkpoints_dir + self.load_name+'_'+str(self.ckpt_num))
                 self.d_saver.restore(self.sess, checkpoints_dir + self.load_name+'_'+str(self.ckpt_num))
@@ -246,10 +247,9 @@ class GENGAN:
             generator_syn =  generate_cpatches(batch_size, ctype=data_type)
 
             self.validate(1, generator_syn, self.sess)
-    
     def synthesis(self,i):
-        with tf.compat.v1.Session() as self.sess:
-            self.sess.run(tf.compat.v1.global_variables_initializer())
+        with tf.Session() as self.sess:
+            self.sess.run(tf.global_variables_initializer())
             if self.ckpt_num is not None:
                 self.g_saver.restore(self.sess, checkpoints_dir + self.load_name+'_'+str(self.ckpt_num))
                 self.d_saver.restore(self.sess, checkpoints_dir + self.load_name+'_'+str(self.ckpt_num))
@@ -282,9 +282,9 @@ class GENGAN:
 
     def synthesize_dataset(self, num, batch_size=batch_size, limits=[None]*c_dims, sample_rates = [0.5, 0.5]):
         num = int(num)
-        with tf.compat.v1.Session() as self.sess:
+        with tf.Session() as self.sess:
             # Load model
-            self.sess.run(tf.compat.v1.global_variables_initializer())
+            self.sess.run(tf.global_variables_initializer())
             if self.ckpt_num is not None:
                 self.g_saver.restore(self.sess, checkpoints_dir + self.load_name+'_'+str(self.ckpt_num))
                 self.d_saver.restore(self.sess, checkpoints_dir + self.load_name+'_'+str(self.ckpt_num))
@@ -292,7 +292,7 @@ class GENGAN:
                 self.g_saver.restore(self.sess, models_dir + self.load_name)
                 self.d_saver.restore(self.sess, models_dir + self.load_name)
 
-            # Create patch generator
+             # Create patch generator
             generator_syn = generate_nc_patches(batch_size, ctype=data_type)
 
             X_train = np.zeros((num, batch_size, self.patch_size, self.patch_size, 1))
